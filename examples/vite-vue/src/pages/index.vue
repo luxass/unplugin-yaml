20f0dadccb4a3295296dda6426c9404b19f84507
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-02 07:48:34 +0200

    chore: move vite vue example

---
+++ examples/vite-vue/src/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { codeToHtml } from "shiki";
+
+import config from "../config.yaml";
+
+const html = await codeToHtml(JSON.stringify(config, null, 2), {
+  lang: "json",
+  themes: {
+    light: "github-light",
+    dark: "github-dark",
+  },
+});
+</script>
+
+<template>
+  <div v-html="html" />
+</template>
