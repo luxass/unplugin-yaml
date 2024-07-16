5bf4fe505aa59396cad02d69cff63254fe9ece72
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-16 14:05:54 +0200

    chore: update deps

--- examples/rspack/src/shims-vue.d.ts
+++ examples/rspack/src/shims-vue.d.ts
@@ -1,6 +1,6 @@
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
-  // biome-ignore lint/complexity/noBannedTypes: reason
+  // eslint-disable-next-line ts/no-empty-object-type
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
