bf00b6caf7f8956e5d9c2f44689d78f8733157fb
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-16 14:12:31 +0200

    test: add rspack test cases

---
+++ test/rspack.test.ts
@@ -0,0 +1,102 @@
+import path from "node:path";
+import { tmpdir } from "node:os";
+import { readFile } from "node:fs/promises";
+import type { Configuration, Stats } from "@rspack/core";
+import { rspack as createRspack } from "@rspack/core";
+import { expect, it } from "vitest";
+
+import YAMLPlugin from "../src/rspack";
+import { removeComments } from "./utils";
+
+const dir = path.join(tmpdir(), "unplugin-yaml-tests");
+async function rspack(config: Configuration): Promise<Stats | undefined> {
+  return new Promise((resolve, reject) => {
+    const compiler = createRspack({
+      optimization: {
+        minimize: true,
+      },
+      output: {
+        path: dir,
+        filename: `${Date.now()}-bundle.js`,
+      },
+      mode: "production",
+      ...config,
+    });
+
+    compiler.run((err, stats) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(stats);
+    });
+  });
+}
+
+it("expect yaml import to be a json object", async () => {
+  const result = await rspack({
+    entry: "./test/fixtures/js-yaml.js",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+
+  const json = result?.toJson();
+  expect(json).toBeDefined();
+
+  const file = json!.assetsByChunkName!.main;
+  const content = await readFile(path.join(dir, file![0]!), "utf-8");
+
+  expect(removeComments(content)).toMatchSnapshot();
+});
+
+it("expect yaml import to be a string", async () => {
+  const result = await rspack({
+    entry: "./test/fixtures/js-yaml-raw.js",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+
+  const json = result?.toJson();
+  expect(json).toBeDefined();
+
+  const file = json!.assetsByChunkName!.main;
+  const content = await readFile(path.join(dir, file![0]!), "utf-8");
+
+  expect(removeComments(content)).toMatchSnapshot();
+});
+
+it("expect yml import to be a json object", async () => {
+  const result = await rspack({
+    entry: "./test/fixtures/js-yml.js",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+
+  const json = result?.toJson();
+  expect(json).toBeDefined();
+
+  const file = json!.assetsByChunkName!.main;
+  const content = await readFile(path.join(dir, file![0]!), "utf-8");
+
+  expect(removeComments(content)).toMatchSnapshot();
+});
+
+it("expect yml import to be a string", async () => {
+  const result = await rspack({
+    entry: "./test/fixtures/js-yml-raw.js",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+
+  const json = result?.toJson();
+  expect(json).toBeDefined();
+
+  const file = json!.assetsByChunkName!.main;
+  const content = await readFile(path.join(dir, file![0]!), "utf-8");
+
+  expect(removeComments(content)).toMatchSnapshot();
+});
