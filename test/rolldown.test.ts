53e8b017ac9c61f6ea017d69e5e75c00a1a0a55a
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-16 14:14:04 +0200

    test: add rolldown test cases

---
+++ test/rolldown.test.ts
@@ -0,0 +1,64 @@
+import { rolldown } from "rolldown";
+import { expect, it } from "vitest";
+import YAMLPlugin from "../src/rolldown";
+import { removeComments } from "./utils";
+
+it("expect yaml import to be a json object", async () => {
+  const bundle = await rolldown({
+    input: "./test/fixtures/yaml-config.yaml",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+  const { output } = await bundle.generate({
+    format: "esm",
+    sourcemap: false,
+  });
+
+  expect(removeComments(output[0].code)).toMatchSnapshot();
+});
+
+it("expect yaml import to be a string", async () => {
+  const bundle = await rolldown({
+    input: "./test/fixtures/js-yaml-raw.js",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+  const { output } = await bundle.generate({
+    format: "esm",
+    sourcemap: false,
+  });
+
+  expect(removeComments(output[0].code)).toMatchSnapshot();
+});
+
+it("expect yml import to be a json object", async () => {
+  const bundle = await rolldown({
+    input: "./test/fixtures/yml-config.yml",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+  const { output } = await bundle.generate({
+    format: "esm",
+    sourcemap: false,
+  });
+
+  expect(removeComments(output[0].code)).toMatchSnapshot();
+});
+
+it("expect yml import to be a string", async () => {
+  const bundle = await rolldown({
+    input: "./test/fixtures/js-yml-raw.js",
+    plugins: [
+      YAMLPlugin(),
+    ],
+  });
+  const { output } = await bundle.generate({
+    format: "esm",
+    sourcemap: false,
+  });
+
+  expect(removeComments(output[0].code)).toMatchSnapshot();
+});
