cb824a97675d204c9eb310d14eb407d3d93b2764
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-01-30 05:34:29 +0100

    test: add tests for vite

---
+++ test/vite.test.ts
@@ -0,0 +1,224 @@
+import { join } from "node:path";
+import { build } from "vite";
+import { describe, expect, it } from "vitest";
+import { fromFileSystem, testdir, type TestdirFromOptions, type TestdirOptions } from "vitest-testdirs";
+import YAMLPlugin from "../src/vite";
+import { removeComments } from "./utils";
+
+describe("handles yaml", () => {
+  it("expect yaml import to be a json object", async () => {
+    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic/yaml"));
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await build({
+      build: {
+        lib: {
+          entry: join(testdirPath, "basic.js"),
+          formats: ["es"],
+          fileName: "bundle",
+          name: "bundle",
+        },
+        outDir: join(testdirPath, "dist"),
+        minify: false,
+      },
+      plugins: [YAMLPlugin()],
+    });
+
+    if (!Array.isArray(result)) {
+      expect.fail("result is not an array");
+    }
+
+    expect(result).toBeDefined();
+
+    const firstResult = result[0];
+
+    expect(firstResult).toBeDefined();
+    expect(firstResult?.output).toBeDefined();
+    expect(firstResult?.output[0]).toBeDefined();
+    expect(firstResult?.output[0].code).toBeDefined();
+    expect(firstResult?.output[0].code).toMatchSnapshot();
+  });
+
+  it("expect yaml import to be a string", async () => {
+    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic/yaml"));
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await build({
+      build: {
+        lib: {
+          entry: join(testdirPath, "basic-raw.js"),
+          formats: ["es"],
+          fileName: "bundle",
+          name: "bundle",
+        },
+        outDir: join(testdirPath, "dist"),
+        minify: false,
+      },
+      plugins: [YAMLPlugin()],
+    });
+
+    if (!Array.isArray(result)) {
+      expect.fail("result is not an array");
+    }
+
+    expect(result).toBeDefined();
+
+    const firstResult = result[0];
+
+    expect(firstResult).toBeDefined();
+    expect(firstResult?.output).toBeDefined();
+    expect(firstResult?.output[0]).toBeDefined();
+    expect(firstResult?.output[0].code).toBeDefined();
+    expect(firstResult?.output[0].code).toMatchSnapshot();
+  });
+});
+
+describe("handles yml", () => {
+  it("expect yml import to be a json object", async () => {
+    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic/yml"));
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await build({
+      build: {
+        lib: {
+          entry: join(testdirPath, "basic.js"),
+          formats: ["es"],
+          fileName: "bundle",
+          name: "bundle",
+        },
+        outDir: join(testdirPath, "dist"),
+        minify: false,
+      },
+      plugins: [YAMLPlugin()],
+    });
+
+    if (!Array.isArray(result)) {
+      expect.fail("result is not an array");
+    }
+
+    expect(result).toBeDefined();
+
+    const firstResult = result[0];
+
+    expect(firstResult).toBeDefined();
+    expect(firstResult?.output).toBeDefined();
+    expect(firstResult?.output[0]).toBeDefined();
+    expect(firstResult?.output[0].code).toBeDefined();
+    expect(firstResult?.output[0].code).toMatchSnapshot();
+  });
+
+  it("expect yml import to be a string", async () => {
+    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic/yml"));
+
+    expect(testdirPath).toBeDefined();
+
+    const result = await build({
+      build: {
+        lib: {
+          entry: join(testdirPath, "basic-raw.js"),
+          formats: ["es"],
+          fileName: "bundle",
+          name: "bundle",
+        },
+        outDir: join(testdirPath, "dist"),
+        minify: false,
+      },
+      plugins: [YAMLPlugin()],
+    });
+
+    if (!Array.isArray(result)) {
+      expect.fail("result is not an array");
+    }
+
+    expect(result).toBeDefined();
+
+    const firstResult = result[0];
+
+    expect(firstResult).toBeDefined();
+    expect(firstResult?.output).toBeDefined();
+    expect(firstResult?.output[0]).toBeDefined();
+    expect(firstResult?.output[0].code).toBeDefined();
+    expect(firstResult?.output[0].code).toMatchSnapshot();
+  });
+});
+
+it("handle multi document", async () => {
+  const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/multi"));
+
+  expect(testdirPath).toBeDefined();
+
+  const result = await build({
+    build: {
+      lib: {
+        entry: join(testdirPath, "multi.js"),
+        formats: ["es"],
+        fileName: "bundle",
+        name: "bundle",
+      },
+      outDir: join(testdirPath, "dist"),
+      minify: false,
+    },
+    plugins: [YAMLPlugin({
+      type: "multi",
+    })],
+  });
+
+  if (!Array.isArray(result)) {
+    expect.fail("result is not an array");
+  }
+
+  expect(result).toBeDefined();
+
+  const firstResult = result[0];
+
+  expect(firstResult).toBeDefined();
+  expect(firstResult?.output).toBeDefined();
+  expect(firstResult?.output[0]).toBeDefined();
+  expect(firstResult?.output[0].code).toBeDefined();
+  expect(firstResult?.output[0].code).toMatchSnapshot();
+});
+
+it("handle transforms", async () => {
+  const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/transform"));
+
+  expect(testdirPath).toBeDefined();
+
+  const result = await build({
+    build: {
+      lib: {
+        entry: join(testdirPath, "transform.js"),
+        formats: ["es"],
+        fileName: "bundle",
+        name: "bundle",
+      },
+      outDir: join(testdirPath, "dist"),
+      minify: false,
+    },
+    plugins: [YAMLPlugin({
+      transform(data) {
+        if (data != null && typeof data === "object" && "this" in data) {
+          return {
+            this: "transformed",
+          };
+        }
+      },
+    })],
+  });
+
+  if (!Array.isArray(result)) {
+    expect.fail("result is not an array");
+  }
+
+  expect(result).toBeDefined();
+
+  const firstResult = result[0];
+
+  expect(firstResult).toBeDefined();
+  expect(firstResult?.output).toBeDefined();
+  expect(firstResult?.output[0]).toBeDefined();
+  expect(firstResult?.output[0].code).toBeDefined();
+  expect(firstResult?.output[0].code).toMatchSnapshot();
+});
