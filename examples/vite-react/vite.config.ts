3de0aca3ec346f4806964b9feaddbcf0ad82a538
Lucas <lucasnrgaard@gmail.com>
2025-01-31 07:41:16 +0100

    chore: cleanup vite example

--- examples/vite-react/vite.config.ts
+++ examples/vite-react/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import yaml from "unplugin-yaml/vite";
 import { defineConfig } from "vite";
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     yaml(),
+    tailwindcss(),
     inspect(),
   ],
 });
